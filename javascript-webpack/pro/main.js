2205b4a4d39b9951ae57c3e1afc5ff382312999d
inki kim <popkim71@gmail.com>
2017-03-16 21:05:03 +0900

    차트생성

--- javascript-webpack/pro/main.js
+++ javascript-webpack/pro/main.js
@@ -47,3 +47,37 @@ $("#chart").kendoChart({
     }]
 
 });
+
+$("#datePicker").kendoDatePicker({
+    value: new Date(),
+    min: new Date(1950, 0, 1),
+    max: new Date(2049, 11, 31)
+})
+
+
+$("#lineChart").kendoChart({
+    title: {
+        text: "Internet Users"
+    },
+    legend: {
+        position: "bottom"
+    },
+    seriesDefaults: {
+        type: "line"
+    },
+    series: [{
+        name: "World",
+        data: [15.7, 16.7, 20, 23.5, 26.6]
+    }, {
+        name: "United States",
+        data: [67.96, 68.93, 75, 74, 78]
+    }],
+    valueAxis: {
+        labels: {
+            format: "{0}%"
+        }
+    },
+    categoryAxis: {
+        categories: [2005, 2006, 2007, 2008, 2009]
+    }
+});
